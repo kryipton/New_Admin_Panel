c0318d053b4cfc3961500096860e2c6c4ec3e862
kryipton <mutalib0101@gmail.com>
2019-10-13 23:09:38 +0400

    fronta problem

---
+++ application/language/en/en_lang.php
@@ -0,0 +1,72 @@
+<?php
+
+
+$lang["ana_sehife"] = "Home Page";
+$lang["adnsu"] = "ASOIU";
+$lang["haqqimizda"] = "About";
+$lang["kafedralar"] = "Departments";
+$lang["elaqe"] = "Contact";
+$lang["labaratoriyalar"] = "Laboratories";
+$lang["bakalavr"] = "Bachelor";
+$lang["magistr"] = "Master";
+$lang["doktorantura"] = "Doctorate";
+$lang["xeberler"] = "News";
+
+
+$lang["muellimler"] = "Teachers";
+$lang["telebeler"] = "Students";
+
+$lang["tedbirler"] = "Events";
+
+$lang["diger"] = "More";
+
+$lang["etrafli"] = "Detailed";
+
+
+$lang["footer"] = "&copy; 2019. All rights reserved.";
+
+$lang["umumi_melumat"] = "General information";
+
+
+$lang["ixtisaslar"] = "Specialties";
+
+
+
+$lang["ixtisas_adi"] = "Specialty name";
+
+$lang["ixtisas_kodu"] = "Specialty code";
+
+
+$lang["bakalavr_tehsili"] = "Bachelor education";
+
+$lang["magistr_tehsili"] = "Master education";
+
+$lang["doktorantura_tehsili"] = "Doctorate education";
+
+$lang["xebere_get"] = "Go To The New";
+
+$lang["dil"] = "Language";
+
+$lang["xidmetler"] = "Services";
+
+$lang["vezife"] = "Position";
+
+$lang["mail"] = "Email";
+
+$lang["elmi_derece"] = "Academic degree";
+
+$lang["diger_xeberler"] = "Other news";
+
+$lang["elm"] = "Knowledge";
+
+$lang["jurnallar"] = "Journal";
+
+$lang["tiraj"] = "Edition";
+
+$lang["dercler"] = "Publication";
+
+$lang["yukle"] = "Download";
+
+
+
+
---
+++ application/language/ru/ru_lang.php
@@ -0,0 +1,75 @@
+<?php
+
+
+$lang["ana_sehife"] = "Главная";
+$lang["adnsu"] = "АГУНП";
+$lang["haqqimizda"] = "Около";
+$lang["kafedralar"] = "Ведомства";
+$lang["elaqe"] = "Контакт";
+$lang["labaratoriyalar"] = "Лаборатории";
+$lang["bakalavr"] = "Холостяк";
+$lang["magistr"] = "Мастер";
+$lang["doktorantura"] = "Докторская степень";
+$lang["xeberler"] = "Новости";
+
+
+$lang["muellimler"] = "Учителей";
+$lang["telebeler"] = "Ученики";
+
+$lang["tedbirler"] = "События";
+
+$lang["diger"] = "Больше";
+
+$lang["etrafli"] = "более";
+
+
+$lang["footer"] = "&copy; 2019. Все права защищены.";
+
+$lang["umumi_melumat"] = "Общая информация";
+
+$lang["ixtisaslar"] = "Специальности";
+
+$lang["ixtisas_adi"] = "Название специальности";
+
+$lang["ixtisas_kodu"] = "Код специальности";
+
+$lang["bakalavr_tehsili"] = "Степень бакалавра";
+
+$lang["magistr_tehsili"] = "Мастер образование";
+
+$lang["doktorantura_tehsili"] = "Докторантура";
+
+$lang["xebere_get"] = "Перейти К Новостям";
+
+$lang["dil"] = "Язк";
+
+$lang["xidmetler"] = "Услуги";
+
+$lang["vezife"] = "Позиция";
+
+$lang["mail"] = "Почта";
+
+$lang["elmi_derece"] = "Ученая степень";
+
+$lang["diger_xeberler"] = "Другие новости";
+
+$lang["elm"] = "Знание";
+
+$lang["jurnallar"] = "Журналы";
+
+$lang["tiraj"] = "Обращение";
+
+$lang["dercler"] = "Опубликованный";
+
+$lang["yukle"] = "Скачать";
+
+
+
+
+
+
+
+
+
+
+
